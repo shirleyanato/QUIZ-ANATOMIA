4fa2e4535d8a6934d4dfd90f4fac673115122f85
shirleyanato <shirleycss2005@gmail.com>
2025-04-06 20:05:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f4f4f4;
+    padding: 20px;
+}
+button {
+    margin: 5px;
+    padding: 10px;
+    cursor: pointer;
+}
